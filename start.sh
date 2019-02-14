ac2819dc7727e821f4b1e26537ddae9713189802
Yang Yongli <yongli.yang@dianping.com>
2019-02-15 00:37:07 +0800

    start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export CONFIG_SERVICE_PASSWORD=password
+export NOTIFICATION_SERVICE_PASSWORD=password
+export STATISTICS_SERVICE_PASSWORD=password
+export ACCOUNT_SERVICE_PASSWORD=password
+export MONGODB_PASSWORD=password
+
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
